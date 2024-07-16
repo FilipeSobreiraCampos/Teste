64e1d82109c9c744de7713b8d725e36543626b1b
Filipe Campos <filipe_sobre1@hotmail.com>
2024-07-16 14:26:21 -0300

    Versionalizando

--- scripts.js
+++ scripts.js
@@ -3,5 +3,5 @@
 // Função para alterar o texto do parágrafo quando o botão é clicado
 document.getElementById('botao').addEventListener('click', function() {
     var paragrafo = document.getElementById('mensagem');
-    paragrafo.textContent = 'Texto alterado com JavaScript!';
+    paragrafo.textContent = 'Texto alterado com JavaScript Filipe meu caro!';
 });
